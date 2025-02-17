9f5b5b1bca251777aa9be8b9fdb888638a566495
Caleb-Alberto <calberto3418@gmail.com>
2025-02-16 20:07:02 -0800

    added emacs package fixed some i3 stuff

--- i3/mouse_settings.sh
+++ i3/mouse_settings.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xinput set-prop 10 "libinput Accel Speed" -0.3
-xinput set-prop 10 "libinput Accel Profile Enabled" 0 1
+xinput set-prop 15 "libinput Accel Speed" -0.45
+xinput set-prop 15 "libinput Accel Profile Enabled" 0 1
