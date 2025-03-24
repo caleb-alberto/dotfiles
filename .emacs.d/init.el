0a152ddcff51ad05a18dad5b7ec97f93f1c3a952
caleb-alberto <calberto3418@gmail.com>
2025-03-23 17:07:26 -0700

    updated emacs config [mac]

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,33 +2,35 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(menu-bar-mode -1)
+(menu-bar-mode 0)
+(column-number-mode 1)
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
+(load-theme 'gruber-darker t)
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'zenburn :no-confirm)
-
-;; org modern
-(use-package org-modern
-  :ensure t
-  :config
-  (global-org-modern-mode 1))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Mononoki Nerd Font" :foundry "UKWN" :slant normal :weight bold :height 146 :width normal))))
- '(org-level-1 ((t (:height 1.5 :weight bold))))
- '(org-level-2 ((t (:height 1.3 :weight bold))))
- '(org-level-3 ((t (:height 1.2 :weight bold))))
- '(org-level-4 ((t (:height 1.1 :weight bold)))))
+ '(default ((t (:family "Mononoki Nerd Font" :foundry "nil" :slant normal :weight regular :height 165 :width normal)))))
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((C . t)
-   (python .t)))
+   (calc . t)
+   (python . t)))
 (setq org-babel-python-command "python3")
+(setq org-link-frame-setup '((file . find-file)))
+(setq org-file-apps
+      '(("\\.pdf\\'" . "open -a Firefox %s")
+	(auto-mode . emacs)))
+
+(defun my-cpp-style ()
+  (setq c-basic-offset 4)
+  (setq indent-tabs-mode nil))
+
+(add-hook 'c++-mode-hook 'my-cpp-style)
+(add-hook 'c-mode-hook 'my-cpp-style)
 
 ;; multi cursor
 (use-package multiple-cursors
@@ -41,6 +43,10 @@
   :ensure t
   :config
   (global-undo-tree-mode 1))
+
+;; magit
+(use-package magit
+  :ensure t)
 
 ;; fuzzy finding
 (use-package ivy
@@ -59,12 +65,23 @@
 (use-package swiper
   :ensure t)
 
+(defun notes()
+  (interactive)
+  (find-file "~/notes/index.org"))
+
 (global-set-key (kbd "C-c C-c") 'mc/edit-lines)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-p") 'counsel-rg)
 (global-set-key (kbd "M-S") 'swiper)
 (global-set-key (kbd "C-SPC") 'set-mark-command)
+
+(setq interprogram-cut-function
+      (lambda (text &optional push)
+    (let* ((process-connection-type nil)
+	   (pbproxy (start-process "pbcopy" "pbcopy" "/usr/bin/pbcopy")))
+      (process-send-string pbproxy text)
+      (process-send-eof pbproxy))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -75,7 +92,7 @@
    '("f87c86fa3d38be32dc557ba3d4cedaaea7bc3d97ce816c0e518dfe9633250e34" default))
  '(display-line-numbers-type 'relative)
  '(global-display-line-numbers-mode t)
- '(package-selected-packages '(multiple-cursors key-chord))
+ '(package-selected-packages '(gruber-darker-theme helm multiple-cursors key-chord))
  '(scroll-bar-mode nil)
+ '(send-mail-function 'mailclient-send-it)
  '(tool-bar-mode nil))
-
